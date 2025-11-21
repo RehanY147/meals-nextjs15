364dc9f39513c9657b7d750c9bbc0e02297505a3
RehanY147 <rehanyousaf147@gmail.com>
2025-11-21 23:19:55 +0500

    Update save meal action and revalidate certain paths

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,10 +1,13 @@
+'use client';
+
 import ImagePicker from '@/components/ImagePicker/ImagePicker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
 import MealSubmitBtn from '@/components/Meals/SubmitBtn/MealSubmitBtn';
+import { useActionState } from 'react';
 
 export default function ShareMealPage() {
-
+  const [state, formAction] = useActionState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -14,7 +17,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,6 +46,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label='Image' name='image'></ImagePicker>
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealSubmitBtn></MealSubmitBtn>
           </p>
